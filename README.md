c45848c3553579ffc529351b29033a83f5461def
ltoddy <taoliu0509@gmail.com>
2020-12-19 10:42:57 +0800

    chroe: add usage

--- README.md
+++ README.md
@@ -4,3 +4,36 @@ redis-rs
 Redis client for Rust.
 
 - Pure Rust, and doesn't depend on any 3rd party libraries
+
+*Cargo.toml*
+```toml
+[dependencies.redisclient]
+version = "*"
+```
+
+*src/main.rs*
+```rust
+extern crate redisclient;
+
+use redisclient::RedisResult;
+use redisclient::client::RedisClient;
+
+fn main() {
+    if let Err(e) = run() {
+        println!("Error -> {}", e);
+    }
+}
+
+fn run() -> RedisResult<()> {
+    let mut client = RedisClient::new();
+    client.mset(vec![("key1", 1), ("key2", 2)])?;
+
+    let values: Vec<String> = client.mget(vec!["key1", "key2"])?;
+    println!("values -> {:?}", values);
+
+    let values: Vec<isize> = client.mget(vec!["key1", "key2"])?;
+    println!("values -> {:?}", values);
+
+    Ok(())
+}
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,37 @@
+//! ```ini
+//! [dependencies.redisclient]
+//! version = "*"
+//! ```
+//!
+//! If you want use the Git version:
+//!
+//! ```ini
+//! [dependencies.redisclient]
+//! git = "https://github.com/ltoddy/redis-rs.git"
+//! ```
+//!
+//! ### simple usage
+//!
+//! ```rust
+//! extern crate redisclient;
+//!
+//! use redisclient::RedisResult;
+//! use redisclient::client::RedisClient;
+//!
+//! fn run() -> RedisResult<()> {
+//!     let mut client = RedisClient::new();
+//!     client.mset(vec![("key1", 1), ("key2", 2)])?;
+//!
+//!     let values: Vec<String> = client.mget(vec!["key1", "key2"])?;
+//!     println!("values -> {:?}", values);
+//!
+//!     let values: Vec<isize> = client.mget(vec!["key1", "key2"])?;
+//!     println!("values -> {:?}", values);
+//!
+//!     Ok(())
+//! }
+//! ```
+
 pub mod client;
 pub mod connection;
 pub mod error;
